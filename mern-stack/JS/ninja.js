27e3b6a5a7b6d6311e42c281bb6dfe53e61b0fb9
baraa-asal <b.aboasal@gmail.com>
2022-12-05 16:46:16 +0200

    ninja

--- mern-stack/JS/ninja.js
+++ mern-stack/JS/ninja.js
@@ -18,6 +18,21 @@ class Ninja {
     }
 }
 
+class Sensei extends Ninja {
+    constructor(name) {
+        super();
+        this.name = name;
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+    speakWisdom() {
+        this.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
+
 const ninja1 = new Ninja("Baraa", 30);
 ninja1.sayName();
 ninja1.showStats();
@@ -27,3 +42,8 @@ const ninja2 = new Ninja("Mostafa", 20)
 ninja2.speed = 4;
 ninja2.strength = 5;
 ninja2.showStats();
+
+
+const superSensei1 = new Sensei("BARAA THE SENSEI");
+superSensei1.speakWisdom();
+superSensei1.showStats();
