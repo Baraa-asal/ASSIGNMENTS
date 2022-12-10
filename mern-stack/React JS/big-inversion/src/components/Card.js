16aa61b615bee47cc989ab6d0c4d1145ddcb9138
baraa-asal <b.aboasal@gmail.com>
2022-12-10 23:17:24 +0200

    big inversion

--- mern-stack/React JS/big-inversion/src/components/Card.js
+++ mern-stack/React JS/big-inversion/src/components/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
     const Card = ({firstname, lastname, age, haircolor} )=>{
-       
+       //i can pass props to Card function, or just I can destructure the variables that i need just like that, and i dont write props.firstname, i use firstname 3atool
         return (
             <div>
                 <h1>{`${lastname}, ${firstname}`}</h1>
