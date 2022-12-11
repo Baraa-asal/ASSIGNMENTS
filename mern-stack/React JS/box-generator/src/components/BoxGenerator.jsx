4dccb4fc37fd4d1d46340885f2e25517f5ac3c0e
baraa-asal <b.aboasal@gmail.com>
2022-12-11 21:40:46 +0200

    Bonus

--- mern-stack/React JS/box-generator/src/components/BoxGenerator.jsx
+++ mern-stack/React JS/box-generator/src/components/BoxGenerator.jsx
@@ -3,11 +3,11 @@ import React, { useState } from 'react'
 const BoxGenerator = () => {
     const [boxes, setBoxes] = useState([]);
     const [color, setColor] = useState("");
+    const [dimension, setDimension] = useState("");
     
     const createBox = () => {
         const temp = [...boxes, color]; //shallow copy of boxes, but adding new elements too
-        setBoxes(temp)
-        
+        setBoxes(temp)        
     }
     const reset = () => {
         const temp = []
@@ -24,13 +24,16 @@ const BoxGenerator = () => {
         <div className='container'>
             <label>Color: </label> 
             <input type="text" value={color} onChange={ (e) =>{setColor(e.target.value)}} />
+            <label>Dimensions: </label> 
+            <input type="text" value={dimension} onChange={ (e) =>{setDimension(e.target.value)}} />
             <button onClick={(e) => createBox()}> Add </button>
             <button onClick={(e) => reset()}> Clear </button>
             <button onClick={(e) => undo()}> Undo </button>
         </div>
+       
         <div className='boxesContainer'>
         {boxes.map(box => 
-        (<div className='box' style={{backgroundColor: box}}></div>)
+        (<div className='box' style={{backgroundColor: box, height: dimension + "px", width: dimension + "px"}}></div>)
         )}
         </div>
 
